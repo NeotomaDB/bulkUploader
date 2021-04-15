c35c423e895b0a50799795c0e77f3ab2d8fc43dc
Simon Goring <simon.j.goring@gmail.com>
2021-04-14 22:43:01 -0700

    The `scrape_dois()` function now allows a savefile. The function saves to RDS file if the user requests, and also reloads.  Reformatted the inputs to reflect the new structure of the query requests.

--- R/scrape_dois.R
+++ R/scrape_dois.R
@@ -2,26 +2,43 @@
 #' @description Using publication information, retrieve a DOI and relevant publication information for a journal article.
 #' @importFrom httr GET content
 #' @importFrom jsonlite fromJSON
-scrape_dois <- function(title, year, n = 20) {
-  url <- "https://api.crossref.org/works"
+#' @import progress
+scrape_dois <- function(x, n = 20, savefile = NA, restore = TRUE) {
 
-  year <-  stringr::str_extract(year, "\\d*")
-  filters <- paste0(paste0("from-pub-date:", year), ",",
-                    paste0("until-pub-date:", year))
+  x$rows = n
+  x$sort="score"
+  x$order="desc"
 
-  result <- httr::GET(url,
-                      query=list(query.bibliographic = title,
-                                 sort = "score",
-                                 order="desc",
-                                 rows = n,
-                                 filter=filters))
-  if (result$status_code == 200) {
-    output <- httr::content(result)
-    totres <- output$message$`total-results`
-    pubs <- output$message$items
-    return(pubs)
-  } else {
-    message("No matched publications.")
-    return(NULL)
+  pb <- progress::progress_bar$new(total=nrow(x))
+
+  pullResult <- function(inp) {
+    url <- "https://api.crossref.org/works"
+    result <- httr::GET(url,
+                      query=inp)
+
+    if (result$status_code == 200) {
+      output <- httr::content(result)
+      totres <- output$message$`total-results`
+      pubs <- output$message$items
+      return(pubs)
+    } else {
+      return(NULL)
+    }
   }
+
+  if(!is.na(savefile) & restore == TRUE) {
+    output <- tryCatch(readRDS(savefile),
+                       error={stop("Could not find savefile to restore from.")})
+  } else {
+    output <- list()
+  }
+
+  for(i in 1:nrow(x)) {
+    pb$tick()
+    output[[i]] <- pullResult(as.list(data.frame(x[i,])))
+    if(!is.na(savefile)) {
+      saveRDS(output, savefile)
+    }
+  }
+  return(output)
 }
