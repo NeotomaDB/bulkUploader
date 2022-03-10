07cd31d5e8a40f1003e9e0187495b9c15a6b6942
dreuter22 <dreuter@uoregon.edu>
2022-03-10 10:49:35 -0800

    aligning cr_to_neotoma with production

--- R/cr_to_neotoma.R
+++ R/cr_to_neotoma.R
@@ -5,51 +5,64 @@
 #' @export
 #'
 cr_to_neotoma <- function(crObject) {
+
   pubs <- map(crObject, function(x) {
 
-    x[is.null(x)] <- NA_character_
+    if (is.na(x)) {
+      pubs <-  new("publication")
+      return(pubs)
+    } else {
 
-    dateParse <- unlist(x$issued) %>%
-      paste(collapse = '/') %>%
-      lubridate::parse_date_time(c('Ym', 'Ymd'), quiet = TRUE)
+        x[is.null(x)] <- NA_character_
 
-    testNull <- function(val, out) {
-      if(is.null(val)) { return(out)} else {return(val)}
+        dateParse <- unlist(x$issued) %>%
+          paste(collapse = '/') %>%
+          lubridate::parse_date_time(c('Ym', 'Ymd'), quiet = TRUE)
+
+        testNull <- function(val, out) {
+          if(is.null(val)) { return(out)} else {return(val)}
+        }
+
+        makeAuthors <- new("authors",
+                           authors = map(x$author, function(y) {
+                            new("author",
+                                author = new("contact",
+                                             familyname =testNull(y$family, NA_character_),
+                                             givennames= testNull(y$given, NA_character_)),
+              order = 1)
+          }))
+
+        testNull <- function(val, out) {
+          if(is.null(val)) { return(out)} else {return(val)}
+        }
+
+        for (j in names(x)) {
+          if (length(x[[j]]) == 0) {
+            x[[j]] <- ""
+          }
+        }
+
+        pubs <- new("publication",
+            publicationtype = testNull(x$type, NA_character_),
+            publicationid = NA_integer_,
+            articletitle = testNull(x$title[[1]], NA_character_),
+            year = lubridate::year(dateParse) %>% as.character(),
+            journal = testNull(x$`container-title`[[1]], NA_character_),
+            volume = testNull(x$volume, NA_character_),
+            issue = testNull(x$`journal-issue`$issue, NA_character_),
+            pages = testNull(x$page, NA_character_),
+            citation = makeCite(paste(pullAuth(makeAuthors), collapse = '; '),
+                                lubridate::year(dateParse),
+                                x$title,
+                                x$`container-title`[[1]],
+                                x$volume,
+                                x$page),
+            doi = testNull(x$DOI, NA_character_),
+            author = makeAuthors)
     }
-
-    makeAuthors <- new("authors",
-                       authors = map(x$author, function(y) {
-                        new("author",
-                            author = new("contact",
-                                         familyname =testNull(y$family, NA_character_),
-                                         givennames= testNull(y$given, NA_character_)),
-          order = 1)
-      }))
-
-    testNull <- function(val, out) {
-      if(is.null(val)) { return(out)} else {return(val)}
-    }
-    if (length(x$title)== 0){
-      x$title <- ""
-      }
-    new("publication",
-        publicationtype = testNull(x$type, NA_character_),
-        publicationid = NA_integer_,
-        articletitle = testNull(x$title[[1]], NA_character_),
-        year = lubridate::year(dateParse) %>% as.character(),
-        journal = testNull(x$`container-title`[[1]], NA_character_),
-        volume = testNull(x$volume, NA_character_),
-        issue = testNull(x$`journal-issue`$issue, NA_character_),
-        pages = testNull(x$page, NA_character_),
-        citation = makeCite(paste(pullAuth(makeAuthors), collapse = '; '),
-                            lubridate::year(dateParse),
-                            x$title,
-                            x$`container-title`[[1]],
-                            x$volume,
-                            x$page),
-        doi = testNull(x$DOI, NA_character_),
-        author = makeAuthors)
+    return(pubs)
   }) %>%
     new("publications", publications = .)
+
   return(pubs)
 }
